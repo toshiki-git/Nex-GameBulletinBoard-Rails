035ac9c5fe1a231f8aae85ea7e813711795a3112
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-09 14:48:58 +0900

    post controller作成

--- backend/app/controllers/posts_controller.rb
+++ backend/app/controllers/posts_controller.rb
@@ -1,2 +1,42 @@
 class PostsController < ApplicationController
+    def index
+        posts = Post.all
+        render json: posts
+    end
+
+    def show
+        post = Post.find params[:id]
+        if post.image.attached?
+            render json: post.as_json.merge(image_url: rails_blob_url(post.image))
+        else
+            render json: post
+        end
+    end
+
+    def create
+        post = Post.new(post_params)
+        post.image.attach(params[:post][:image])
+        if post.save
+            render json: post
+        else
+            render json: post.errors, status: :unprocessable_entity
+        end
+    end
+
+    def update
+        post = Post.find params[:id]
+        post.update post_params
+        render json: post
+    end
+
+    def destroy
+        post = Post.find params[:id]
+        post.destroy
+        render json: post
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:user_id, :content, :hashtags, :image)
+    end
 end
