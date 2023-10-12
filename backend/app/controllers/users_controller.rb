113d7b636f2bbeec8bd4057ab2927f1237d61c80
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-12 17:22:45 +0900

    画像投稿API作成とリファクタリング

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,63 +1,86 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :update, :destroy, :upload_image]
     skip_before_action :authenticate_request, only: [:create]
-
+  
     def index
-        @users = User.all
-        render json: @users
+      @users = User.all.map do |user|
+        user_attributes(user)
+      end
+      render json: @users
     end
-
+  
     def show
-        @user = User.find params[:id]
-        render json: @user
+      render json: user_attributes(@user)
     end
-
+  
     def create
-        @user = User.new(user_params)
-        
-        if @user.save
-          token = JwtService.encode({ user_id: @user.id })
-          render json: { user: @user, token: token }, status: :created
-        else
-          render json: @user.errors, status: :unprocessable_entity
-        end
+      @user = User.new(user_params)
+      @user.image.attach(params[:user][:image])
+      if @user.save
+        token = JwtService.encode({ user_id: @user.id })
+        render json: { user: @user, token: token }, status: :created
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
-     
+  
     def update
-        @user = User.find params[:id]
-        @user.update user_params
+      if @user.update(user_params)
         render json: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
     end
-
+  
     def destroy
-        @user = User.find params[:id]
-        @user.destroy
-        render json: @user
+      @user.destroy
+      render json: { message: 'User deleted successfully' }
     end
-
+  
     def me
-        token = cookies.signed[:user_token]
-        return render json: { error: 'Token is missing' }, status: :unauthorized unless token
-
-        decoded_payload = JwtService.decode(token)
-        return render json: { error: 'Invalid token' }, status: :unauthorized unless decoded_payload
-
-        user_id = decoded_payload['user_id']
-        @user = User.find_by(id: user_id)
-        if @user
-            render json: @user
-        else
-            render json: { error: 'User not found' }, status: :not_found
-        end
+      token = cookies.signed[:user_token]
+      return render json: { error: 'Token is missing' }, status: :unauthorized unless token
+  
+      decoded_payload = JwtService.decode(token)
+      return render json: { error: 'Invalid token' }, status: :unauthorized unless decoded_payload
+  
+      user_id = decoded_payload['user_id']
+      @user = User.find_by(id: user_id)
+      if @user
+        render json: @user
+      else
+        render json: { error: 'User not found' }, status: :not_found
+      end
     end
-
+  
+    def upload_image
+      if @user.update(user_params)
+        render json: { message: 'Image uploaded successfully' }
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+  
     def current
-        render json: @current_user
+      render json: @current_user
     end
-
+  
     private
-
-    def user_params
-        params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  
+    def set_user
+      @user = User.find(params[:id])
     end
-end
-
+  
+    def user_attributes(user)
+      if user.image.attached?
+        user.as_json.merge(image_url: rails_blob_url(user.image))
+      else
+        user.as_json
+      end
+    end
+  
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :image)
+    end
+  end
+  
