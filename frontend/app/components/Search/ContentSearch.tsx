637312601e522f14f39ac74afafe7e4a3b1cae0f
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-16 12:34:22 +0900

    SearchPostにページネーション

--- frontend/app/components/Search/ContentSearch.tsx
+++ frontend/app/components/Search/ContentSearch.tsx
@@ -1,44 +1,38 @@
 "use client";
 import React, { useState } from "react";
 import SearchCSS from "./Search.module.scss";
-import axios from "@/lib/axios";
-import { PostType } from "@/lib/types";
+import SearchPostList from "../Post/SearchPostList";
 
-interface ContentSearchProps {
-  setShowResults: (value: boolean) => void;
-  setSearchResults: React.Dispatch<React.SetStateAction<PostType[]>>;
-}
-
-const ContentSearch: React.FC<ContentSearchProps> = ({
-  setShowResults,
-  setSearchResults,
-}) => {
+const ContentSearch = () => {
   const [query, setQuery] = useState("");
+  const [activeQuery, setActiveQuery] = useState("");
 
-  const handleSearch = async () => {
-    setShowResults(false);
-    const res = await axios.get(`posts/search?query=${query}`);
-    setSearchResults(res.data);
-    setShowResults(true);
+  const handleSearch = () => {
+    setActiveQuery(query);
   };
 
   return (
-    <div className={`${SearchCSS.content}`}>
-      <input
-        type="text"
-        value={query}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setQuery(e.target.value)
-        }
-        placeholder="検索..."
-        className={`${SearchCSS.input} p-2 border border-foreground rounded bg-overlay`}
-      />
-      <button
-        onClick={handleSearch}
-        className="ml-2 p-2 bg-primary text-white rounded-full w-20"
-      >
-        検索
-      </button>
+    <div>
+      <div className={`${SearchCSS.content}`}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setQuery(e.target.value)
+          }
+          placeholder="検索..."
+          className={`${SearchCSS.input} p-2 border border-foreground rounded bg-overlay`}
+        />
+        <button
+          onClick={handleSearch}
+          className="ml-2 p-2 bg-primary text-white rounded-full w-20"
+        >
+          検索
+        </button>
+      </div>
+      <div>
+        <SearchPostList query={activeQuery} />
+      </div>
     </div>
   );
 };
--- frontend/app/search/page.tsx
+++ frontend/app/search/page.tsx
@@ -2,21 +2,12 @@
 import React from "react";
 import ContentSearch from "@/app/components/Search/ContentSearch";
 import SearchIcon from "@/app/components/Search/SearchIcon";
-import { PostType } from "@/lib/types";
-import SearchPostList from "../components/Post/SearchPostList";
 
 const Search = () => {
-  const [showResults, setShowResults] = React.useState(false);
-  const [searchResults, setSearchResults] = React.useState<PostType[]>([]);
-
   return (
     <div>
       <SearchIcon />
-      <ContentSearch
-        setShowResults={setShowResults}
-        setSearchResults={setSearchResults}
-      />
-      {showResults ? <SearchPostList posts={searchResults} /> : null}
+      <ContentSearch />
     </div>
   );
 };
