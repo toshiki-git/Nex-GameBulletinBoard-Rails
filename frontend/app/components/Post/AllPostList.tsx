d410abb70504596b767d7136dfc6c14612337c8b
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-16 12:33:34 +0900

    AllPostにページネーション

--- frontend/app/components/Post/AllPostList.tsx
+++ frontend/app/components/Post/AllPostList.tsx
@@ -4,6 +4,7 @@ import axios from "@/lib/axios";
 import PostDisplay from "./PostDisplay";
 import Link from "next/link";
 import { PostType } from "@/lib/types";
+import { Pagination } from "@nextui-org/react";
 
 interface PostListProps {
   newPost?: PostType;
@@ -13,12 +14,16 @@ const AllPostList: React.FC<PostListProps> = ({ newPost }) => {
   const [posts, setPosts] = useState<PostType[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [postsCount, setPostsCount] = useState<number>(0);
+  const pagination = 10;
 
   useEffect(() => {
     const fetchAllPosts = async () => {
       try {
-        const response = await axios.get("/posts/");
-        setPosts(response.data.reverse());
+        const response = await axios.get(`/posts?page=${currentPage}`);
+        setPosts(response.data.posts);
+        setPostsCount(response.data.total_posts);
       } catch (err) {
         setError("データの取得に失敗しました。");
         console.error("Error fetching posts:", err);
@@ -28,7 +33,8 @@ const AllPostList: React.FC<PostListProps> = ({ newPost }) => {
     };
 
     fetchAllPosts();
-  }, []);
+    window.scrollTo(0, 0);
+  }, [currentPage]);
 
   useEffect(() => {
     if (newPost) {
@@ -51,6 +57,17 @@ const AllPostList: React.FC<PostListProps> = ({ newPost }) => {
           />
         </Link>
       ))}
+      <div className="flex flex-col gap-5 justify-center items-center mt-4">
+        <div className="flex justify-center w-full">
+          <Pagination
+            showControls
+            total={Math.ceil(postsCount / pagination)}
+            color="secondary"
+            page={currentPage}
+            onChange={setCurrentPage}
+          />
+        </div>
+      </div>
     </div>
   );
 };
