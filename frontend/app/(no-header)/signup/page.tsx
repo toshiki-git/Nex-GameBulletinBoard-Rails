d9ca5acec9fea775e9aecb43d32763379db0f660
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-17 14:13:58 +0900

    コンポーネントの呼び出し

--- frontend/app/(no-header)/signup/page.tsx
+++ frontend/app/(no-header)/signup/page.tsx
@@ -4,6 +4,7 @@ import { Button, Card, CardBody, CardHeader, Input } from "@nextui-org/react";
 import { AiFillEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { useRouter } from "next/navigation";
 import axios from "@/lib/axios";
+import Welcom from "@/app/components/Login/Welcom";
 
 const SignUp = () => {
   const router = useRouter();
@@ -52,82 +53,90 @@ const SignUp = () => {
     setIsPassConfVisible(!isPassConfVisible);
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card>
-        <CardHeader>
-          <h1>新規登録</h1>
-        </CardHeader>
-        <CardBody>
-          <div>
-            <Input
-              label="User Name"
-              variant="bordered"
-              placeholder="Enter your user name"
-              className="max-w-xs mb-4"
-              value={userName}
-              onValueChange={setUserName}
-            />
-            <Input
-              type="email"
-              label="Email"
-              variant="bordered"
-              placeholder="Enter your email"
-              className="max-w-xs mb-4"
-              value={email}
-              onValueChange={setEmail}
-            />
-            <Input
-              label="Password"
-              variant="bordered"
-              placeholder="Enter your password"
-              value={password}
-              onValueChange={setPassword}
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={togglePassVisibility}
+    <div>
+      <Welcom />
+      <div className="flex justify-center items-center">
+        <Card>
+          <CardHeader>
+            <div className="text-lg">新規登録</div>
+          </CardHeader>
+          <CardBody>
+            <div>
+              <Input
+                label="User Name"
+                variant="bordered"
+                placeholder="Enter your user name"
+                className="max-w-xs mb-4"
+                value={userName}
+                onValueChange={setUserName}
+              />
+              <Input
+                type="email"
+                label="Email"
+                variant="bordered"
+                placeholder="Enter your email"
+                className="max-w-xs mb-4"
+                value={email}
+                onValueChange={setEmail}
+              />
+              <Input
+                label="Password"
+                variant="bordered"
+                placeholder="Enter your password"
+                value={password}
+                onValueChange={setPassword}
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={togglePassVisibility}
+                  >
+                    {isPassVisible ? (
+                      <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <AiFillEye className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isPassVisible ? "text" : "password"}
+                className="max-w-xs mb-4"
+              />
+              <Input
+                label="Password Confirmation"
+                variant="bordered"
+                placeholder="Enter your password again"
+                value={passwordConf}
+                onValueChange={setPasswordConf}
+                endContent={
+                  <button
+                    className="focus:outline-none"
+                    type="button"
+                    onClick={togglePassConfVisibility}
+                  >
+                    {isPassConfVisible ? (
+                      <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
+                    ) : (
+                      <AiFillEye className="text-2xl text-default-400 pointer-events-none" />
+                    )}
+                  </button>
+                }
+                type={isPassConfVisible ? "text" : "password"}
+                className="max-w-xs mb-4"
+              />
+              <div className="flex justify-center">
+                <Button
+                  onClick={handleSubmit}
+                  color="primary"
+                  radius="full"
+                  className="transition-transform duration-100 ease-in-out transform hover:scale-105"
                 >
-                  {isPassVisible ? (
-                    <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
-                  ) : (
-                    <AiFillEye className="text-2xl text-default-400 pointer-events-none" />
-                  )}
-                </button>
-              }
-              type={isPassVisible ? "text" : "password"}
-              className="max-w-xs mb-4"
-            />
-            <Input
-              label="Password Confirmation"
-              variant="bordered"
-              placeholder="Enter your password again"
-              value={passwordConf}
-              onValueChange={setPasswordConf}
-              endContent={
-                <button
-                  className="focus:outline-none"
-                  type="button"
-                  onClick={togglePassConfVisibility}
-                >
-                  {isPassConfVisible ? (
-                    <AiOutlineEyeInvisible className="text-2xl text-default-400 pointer-events-none" />
-                  ) : (
-                    <AiFillEye className="text-2xl text-default-400 pointer-events-none" />
-                  )}
-                </button>
-              }
-              type={isPassConfVisible ? "text" : "password"}
-              className="max-w-xs mb-4"
-            />
-            <div className="flex justify-center">
-              <Button onClick={handleSubmit} color="primary">
-                Register
-              </Button>
+                  Register
+                </Button>
+              </div>
             </div>
-          </div>
-        </CardBody>
-      </Card>
+          </CardBody>
+        </Card>
+      </div>
     </div>
   );
 };
