50d0cd782b20d31653a60815aa49c74bbfdddc35
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-18 18:14:50 +0900

    NotificationListコンポーネント作成

--- frontend/app/components/Notification/NotificationList.tsx
+++ frontend/app/components/Notification/NotificationList.tsx
@@ -1,14 +1,38 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "@/lib/axios";
 import NotificationItem from "./NotificationItem";
-import notificationsData from "./temporary_post_data.json";
+import { NotificationDataType } from "@/lib/types";
 
 const NotificationList = () => {
+  // 状態に通知のリストを保持します。
+  const [notifications, setNotifications] = useState<NotificationDataType[]>(
+    []
+  );
+
+  useEffect(() => {
+    // この関数は、APIから通知を取得します。
+    const fetchNotifications = async () => {
+      try {
+        // あなたのAPIエンドポイントに変更してください。
+        const response = await axios.get("/notifications");
+
+        setNotifications(response.data);
+      } catch (error) {
+        console.error("There was an error fetching the notifications!", error);
+      }
+    };
+
+    fetchNotifications();
+  }, []);
+
   return (
     <div className="max-w-lg mx-auto mt-8 mb-8 p-4">
-      {notificationsData.map((notification) => (
+      {notifications.map((notification) => (
         <NotificationItem
           key={notification.id}
-          content={notification.content}
-          avatarSrc="https://avatars.githubusercontent.com/u/30373425?v=4"
+          content={notification.notifiable.content}
+          image_url={notification.actor.image_url}
         />
       ))}
     </div>
