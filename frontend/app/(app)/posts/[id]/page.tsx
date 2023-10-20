2415c96d1a38ed7ac430076fae9ad174ca8dad73
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-20 16:10:19 +0900

    use clientを修正

--- frontend/app/(app)/posts/[id]/page.tsx
+++ frontend/app/(app)/posts/[id]/page.tsx
@@ -1,50 +1,11 @@
-"use client";
-import React, { useEffect, useState } from "react";
-import axios from "@/lib/axios";
-import { PostType } from "@/lib/types";
-import PostDisplay from "@/app/components/Post/PostDisplay";
-import { Spinner } from "@nextui-org/react";
 import CommentArea from "@/app/components/Comment/CommentArea";
-import CommentDisplay from "@/app/components/Comment/CommnetDisplay";
 import CommentList from "@/app/components/Comment/CommentList";
+import CommentPostDisplay from "@/app/components/Comment/CommentPostDisplay";
 
 const Post = ({ params }: { params: { id: string } }) => {
-  const [post, setPost] = useState<PostType>({} as PostType);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const response = await axios.get(`/posts/${params.id}`);
-        setPost(response.data);
-      } catch (err) {
-        setError("データの取得に失敗しました。");
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchPosts();
-  }, []);
-
-  if (isLoading)
-    return (
-      <div className="mt-3 flex justify-center">
-        <Spinner label="Loading..." color="primary" />
-      </div>
-    );
-  if (error) return <p>{error}</p>;
-
   return (
     <>
-      <PostDisplay
-        content={post.content}
-        hashtags={post.hashtags}
-        image={post.image_url}
-        userId={post.user_id}
-      />
-
+      <CommentPostDisplay postId={parseInt(params.id)} />
       <CommentArea postId={parseInt(params.id)} />
       <CommentList postId={parseInt(params.id)} />
     </>
--- frontend/app/components/Comment/CommentList.tsx
+++ frontend/app/components/Comment/CommentList.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import axios from "@/lib/axios";
 import { CommentDataType } from "@/lib/types";
