d6b9ce2dad6ea64cb537ae755bfab5fc1e2c4f8e
Toshiki Takakubo <219tosiki@gmail.com>
2023-10-31 15:28:03 +0900

    axiosのheaderにtokenを設定

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -5,6 +5,11 @@ const axios = Axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT as string,
   headers: {
     "Content-Type": "application/json",
+    ...(document.cookie.includes("user_token=") && {
+      Authorization: `Bearer ${
+        document.cookie.split("user_token=")[1].split(";")[0]
+      }`,
+    }),
   },
 });
 
